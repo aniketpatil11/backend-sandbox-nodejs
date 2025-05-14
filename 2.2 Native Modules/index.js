c0cc4a5656a4c28e72a69764249a05bdf11c2977
aniketpatil11 <aniket112.patil@gmail.com>
2025-05-14 15:24:34 +0530

    2.2Nodejs - NPM and node modules qr

--- 2.2 Native Modules/index.js
+++ 2.2 Native Modules/index.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 
 // Creates file in system
-// fs.writeFile("message.txt", "Hello from nodeJS. I am writing here", (err) => {
-//     if(err) throw err; 
-//     console.log("File has been saved");
-// });
+fs.writeFile("message.txt", "Hello from nodeJS. I am writing here", (err) => {
+    if(err) throw err; 
+    console.log("File has been saved");
+});
 
 // Readfile challenge
 fs.readFile("./message.txt", "utf-8", (err, data) => {

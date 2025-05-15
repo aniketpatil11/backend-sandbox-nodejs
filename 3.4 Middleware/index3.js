727b7efe9f96bd12f2fab90c1a87ad1f2f359522
aniketpatil11 <aniket112.patil@gmail.com>
2025-05-15 18:28:41 +0530

    3.4Express - customMiddlewares, Middlewares, morgan, parser, filetopath, dirname

--- 3.4 Middleware/index3.js
+++ 3.4 Middleware/index3.js
@@ -3,6 +3,13 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+// Custome middleware
+function logger(req, res, next){
+  console.log("Requested Method:", req.method);
+  console.log("Requested URL:", req.url);
+  next();
+}
+
 app.use(logger);
 
 app.get("/", (req, res) => {
--- 3.4 Middleware/index4.js
+++ 3.4 Middleware/index4.js
@@ -1,7 +1,33 @@
 import express from "express";
+import { dirname } from "path"; 
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
+let brandName = ""
+
+app.use(express.urlencoded({ extended: true }));
+
+// Define the middleware:
+function bradNameGenerator(req, res, next){
+  console.log(req.body);
+  brandName = req.body["street"] + req.body["pet"]; 
+  next();
+}
+
+// Use the middleware:
+app.use(bradNameGenerator);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/submit", (req, res) => {
+  res.send(`<h1>The brand name is: </h1> </br> <h3> ${brandName} </h3>`);
+  //res.sendStatus(200);
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
